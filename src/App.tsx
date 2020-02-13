e892a9955e43c40bf2e5cc87b8d9c5816ace96a5
JustinJefferson <justleland@gmail.com>
2020-02-13 14:04:43 -0500

    Navigator takes an array of strings and generates a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
+import Navigator from './components/Navigator'
+
+const links: string[] = [
+  'Info',
+  'Register',
+  'Media',
+  'FAQ'
+]
 
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <Navigator links={links} />
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/info">Info</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/media">Media</Link>
-            </li>
-            <li>
-              <Link to="/faq">FAQ</Link>
-            </li>
-          </ul>
           <Switch>
             <Route path="/"></Route>
             <Route path="/info"></Route>
