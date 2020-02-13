d29ec547f8a3643b5e9ac7a40cb216f4b4e2ae0d
JustinJefferson <justleland@gmail.com>
2020-02-12 21:05:31 -0500

    Intialized repo for ski weekend react site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/info">Info</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/media">Media</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/"></Route>
+            <Route path="/info"></Route>
+            <Route path="/register"></Route>
+            <Route path="/media"></Route>
+            <Route path="/faq"></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
