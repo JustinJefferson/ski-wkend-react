f95c46ab2c218c6fcf55f5704f6ee0ad8d6f936e
JustinJefferson <justleland@gmail.com>
2020-03-10 23:42:09 -0400

    Added extra CSS styling

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import './Info.css';
+import Panel from '../components/Panel';
 
 const Info = () =>
     <div>
-        <h3>This is the Info page</h3>
+        <Panel look='white'>
+            <div>
+                <p>
+                    This is a panel section. It displays text on a board that crosses the entire web page.
+                    A good use for a section like this is to get peoples attention on a home page. It 
+                    should be exciting and riveting to look at.
+                </p>
+            </div>
+        </Panel>
+        <Panel look='teal'>
+            <div>
+                <p>
+                    This is a panel section. It displays text on a board that crosses the entire web page.
+                    A good use for a section like this is to get peoples attention on a home page. It 
+                    should be exciting and riveting to look at.
+                </p>
+            </div>
+        </Panel>
+        <Panel look='purple'>
+            <div>
+                <p>
+                    This is a panel section. It displays text on a board that crosses the entire web page.
+                    A good use for a section like this is to get peoples attention on a home page. It 
+                    should be exciting and riveting to look at.
+                </p>
+            </div>
+        </Panel>
+        <Panel look='yellow'>
+            <div>
+                <p>
+                    This is a panel section. It displays text on a board that crosses the entire web page.
+                    A good use for a section like this is to get peoples attention on a home page. It 
+                    should be exciting and riveting to look at.
+                </p>
+            </div>
+        </Panel>
+        <Panel look='clear'>
+            <div>
+                <p>
+                    This is a panel section. It displays text on a board that crosses the entire web page.
+                    A good use for a section like this is to get peoples attention on a home page. It 
+                    should be exciting and riveting to look at.
+                </p>
+            </div>
+        </Panel>
     </div>
 
 export default Info;
